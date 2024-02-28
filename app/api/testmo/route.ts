802d9398f75e3ba32e9159ba9d8afc211abdb9a1
shumakmanohar <shumakmanohar123@gmail.com>
2024-02-28 19:04:08 +0530

    Modified Testmo Api route

--- app/api/testmo/route.ts
+++ app/api/testmo/route.ts
@@ -13,7 +13,6 @@ export async function GET(request: Request) {
 		headers: {
 			"Access-Control-Allow-Origin": "*",
 			"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-			"Access-Control-Allow-Headers": "Content-Type, Authorization",
 		},
 	});
 }
