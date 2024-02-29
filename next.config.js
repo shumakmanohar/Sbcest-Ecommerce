3b400db67d463b30fcfc8e51d933b86511be3596
shumakmanohar <shumakmanohar123@gmail.com>
2024-02-29 10:19:58 +0530

    modified nextjs config from docs

--- next.config.js
+++ next.config.js
@@ -6,18 +6,6 @@ const withNextIntl = createNextIntlPlugin(
 );
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "images.unsplash.com",
-			},
-			{
-				protocol: "https",
-				hostname: "mediacdn.sbcest.com",
-			},
-		],
-	},
 	async headers() {
 		return [
 			{
@@ -33,11 +21,24 @@ const nextConfig = {
 					},
 					{
 						key: "Access-Control-Allow-Headers",
-						value: "Content-Type, Authorization",
+						value:
+							"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
 					},
 				],
 			},
 		];
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "images.unsplash.com",
+			},
+			{
+				protocol: "https",
+				hostname: "mediacdn.sbcest.com",
+			},
+		],
 	},
 };
 
