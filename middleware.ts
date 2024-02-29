b69e9b1245e728d44978688485f259800bf9de81
shumakmanohar <shumakmanohar123@gmail.com>
2024-02-29 10:41:12 +0530

    Rolled back old middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,34 @@
+import { authMiddleware } from "@clerk/nextjs";
+import createMiddleware from "next-intl/middleware";
+
+const intlMiddleware = createMiddleware({
+	locales: ["en", "ar"],
+	defaultLocale: "en",
+});
+
+export default authMiddleware({
+	beforeAuth: (req) => {
+		// Execute next-intl middleware before Clerk's auth middleware
+		if (
+			req.nextUrl.pathname.startsWith("/_next") ||
+			req.nextUrl.pathname.includes("/api/")
+		) {
+			return;
+		}
+		return intlMiddleware(req);
+	},
+	publicRoutes: [
+		"((?!^/cms/).*)",
+		"((?!^/checkout/).*)",
+		"((?!^/dashboard/).*)",
+	],
+});
+
+export const config = {
+	matcher: [
+		"/((?!.+\\.[\\w]+$|_next).*)",
+		"/",
+		"/(api|trpc)(.*)",
+		"/(ar|en)/:path*",
+	],
+};
